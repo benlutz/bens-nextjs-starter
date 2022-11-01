6912519b601d6aee5287ed32f1f651081aff22d9
Benjamin Lutz <benni.lutz@gmail.com>
2022-11-01 13:48:02 +0100

    put all in src folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import SEO from '../next-seo.config'
+import SEO from '../../next-seo.config'
 import { DefaultSeo } from 'next-seo'
 
 export default function App({ Component, pageProps }: AppProps) {
