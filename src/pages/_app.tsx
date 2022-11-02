75765702cb4f072d89f0e17b57248a82bbde54e6
Benjamin Lutz <benni.lutz@gmail.com>
2022-11-02 18:44:01 +0100

    add jest testing framework

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,25 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import SEO from "../../next-seo.config";
-import { DefaultSeo } from "next-seo";
-import { CookieNotice } from "../features/cookie-consent/CookieNotice";
-import styles from "../styles/Home.module.css";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import SEO from '../../next-seo.config'
+import { DefaultSeo } from 'next-seo'
+import { CookieNotice } from '../features/cookie-consent/CookieNotice'
+import styles from '../styles/Home.module.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <DefaultSeo {...SEO} />
-      <footer className={styles.footer}>
+      <footer className={styles.footer} data-testid='footer'>
         <a
-          href="https://www.benjaminlutz.at"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://www.benjaminlutz.at'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Powered by Benjamin Lutz
         </a>
       </footer>
       <CookieNotice />
     </>
-  );
+  )
 }
