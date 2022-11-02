52310fb14bdfc3f289c2245d85338c136eaad440
Benjamin Lutz <benni.lutz@gmail.com>
2022-11-02 12:46:45 +0100

    add simple working cookie notice

---
+++ src/features/cookie-consent/helpers.ts
@@ -0,0 +1,37 @@
+import config from './cookie-consent.config'
+import Cookies from 'js-cookie'
+import { Consent, CookieId, CookieOption } from './types'
+
+export const getConsentCookie = () => {
+  const consentCookie = Cookies.get(config.consentCookieName)
+  if (consentCookie) return JSON.parse(consentCookie)
+}
+
+export const setConsentCookie = (consent: Consent) => {
+  consent.consentGivenAt = new Date()
+  Cookies.set(config.consentCookieName, JSON.stringify(consent), {
+    path: config.consentCookiePath,
+    expires: config.consentCookieExpiresIn,
+  })
+}
+
+export const removeConsentCookie = () => {
+  Cookies.remove(config.consentCookieName)
+}
+
+export const hasConsentCookie = () => {
+  if (getConsentCookie()) return true
+  else return false
+}
+
+export const fireCallbacks = () => {
+  if (!hasConsentCookie()) return
+
+  config.cookies?.map((cookie) => {
+    if (getConsentCookie().cookies.includes(cookie.id)) cookie.callback?.()
+  })
+}
+
+export const hasConsentFor = (cookieId: CookieId) => {
+  return getConsentCookie()?.cookies?.includes(cookieId)
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,24 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import SEO from '../../next-seo.config'
 import { DefaultSeo } from 'next-seo'
+import { CookieNotice } from '../features/cookie-consent/CookieNotice'
+import styles from '../styles/Home.module.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <DefaultSeo {...SEO} />
+      <footer className={styles.footer}>
+        <a
+          href='https://www.benjaminlutz.at'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Powered by Benjamin Lutz
+        </a>
+      </footer>
+      <CookieNotice />
     </>
   )
 }
