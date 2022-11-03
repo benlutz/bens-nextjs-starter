b1b535cd1bc004b7d3cb8de3f1a5ad9060e48372
Benjamin Lutz <benni.lutz@gmail.com>
2022-11-03 12:08:59 +0100

    add md pages support

--- src/features/content/helpers.ts
+++ src/features/content/helpers.ts
@@ -1,76 +1,76 @@
-import path from "path";
-import matter from "gray-matter";
-import { remark } from "remark";
-import html from "remark-html";
-import fs from "fs";
+import path from 'path'
+import matter from 'gray-matter'
+import { remark } from 'remark'
+import html from 'remark-html'
+import fs from 'fs'
 
-type ContentType = "articles" | "projects";
+type ContentType = 'articles' | 'projects' | 'pages'
 
 const computeContentDirectory = (type: ContentType) => {
-  return path.join(process.cwd(), "src", "content", type);
-};
+  return path.join(process.cwd(), 'src', 'content', type)
+}
 
 export const getAllContentSlugs = (type: ContentType) => {
-  const paths: any = [];
+  const paths: any = []
 
   fs.readdirSync(computeContentDirectory(type)).map((fileName: any) => {
     paths.push({
       params: {
-        slug: fileName.replace(/\.md$/, ""),
+        slug: fileName.replace(/\.md$/, ''),
       },
-    });
-  });
+    })
+  })
 
-  return paths;
-};
+  return paths
+}
 
 export const getAllContentData = (type: ContentType) => {
-  const fileNames = fs.readdirSync(computeContentDirectory(type));
+  const fileNames = fs.readdirSync(computeContentDirectory(type))
 
   const allPostsData = fileNames.map((fileName) => {
     // Remove ".md" from file name to get id
-    const slug = fileName.replace(/\.md$/, "");
+    const slug = fileName.replace(/\.md$/, '')
 
     // Read markdown file as string
     const fullPath = path.join(
-      path.join(process.cwd(), "src", "content", type),
+      path.join(process.cwd(), 'src', 'content', type),
       fileName
-    );
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+    )
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
 
     // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+    const matterResult = matter(fileContents)
 
     // Combine the data with the slug
     return {
       slug,
       ...matterResult.data,
-    };
-  });
-  return allPostsData;
-};
+    }
+  })
+  return allPostsData
+}
 
 export const getContentData = async (
   type: ContentType,
   slug: StringConstructor
 ) => {
-  const dir = computeContentDirectory(type);
-  const fullPath = path.join(dir, `${slug}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const dir = computeContentDirectory(type)
+  const fullPath = path.join(dir, `${slug}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
 
   // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents);
+  const matterResult = matter(fileContents)
 
   // Use remark to convert markdown into HTML string
   const processedContent = await remark()
     .use(html)
-    .process(matterResult.content);
-  const contentHtml = processedContent.toString();
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
 
   // Combine the data with the id and contentHtml
   return {
     slug,
     contentHtml,
     ...matterResult.data,
-  };
-};
+  }
+}
---
+++ src/pages/[slug].tsx
@@ -0,0 +1,53 @@
+import { getAllContentSlugs, getContentData } from '../features/content/helpers'
+
+const Page = ({ page }: any) => {
+  return (
+    <>
+      <div style={styles.headerContainer}>
+        <h1 style={styles.title}>{page.title}</h1>
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: page.contentHtml }} />
+    </>
+  )
+}
+
+export default Page
+
+export async function getStaticPaths() {
+  const paths = getAllContentSlugs('pages')
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps(props: any) {
+  // Fetch necessary data for the blog post using params.slug
+  const {
+    params: { slug },
+  } = props
+  const page = await getContentData('pages', slug)
+  return {
+    props: {
+      page,
+    },
+  }
+}
+
+const styles = {
+  headerContainer: {
+    backgroundImage: 'linear-gradient(145deg,#ff6600 0%,#ff9651 100%)',
+    color: 'white',
+    padding: '25px',
+    marginBottom: '50px',
+  },
+  title: {
+    fontSize: '30px',
+    fontWeight: '200',
+    paddingBottom: '0px',
+    marginBottom: '0px',
+  },
+  subtitle: {},
+  backgroundBlendMode: 'color-burn',
+}
